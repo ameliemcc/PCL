069287f85b42971678fb962b2ceb6191c56b5e5b
amélie <amelie.mccormick@unil.ch>
2021-12-28 22:12:57 +0100

    made a few changes to the parse_corpus.py file & added the parsed was files

--- parse_corpus.py
+++ parse_corpus.py
@@ -7,13 +7,15 @@ command-line:
 python \txt_to_spacy.py file1 file2 file3
 """
 
-
 import spacy, sys
 from spacy.tokens import DocBin
 nlp = spacy.load('en_core_web_sm')
 # files = ['off_news_2014_corpus.txt', ...  ]
-files = (sys.argv[1], sys.argv[2], sys.argv[3])
+files = (sys.argv[1:])
+
+
 def txt_to_disk(file_tuple):
+    """ parses the corpora with SpaCy and creates .spacy files with the output. """
     i = 1
     for file in file_tuple:
         docbin = DocBin()
@@ -21,9 +23,9 @@ def txt_to_disk(file_tuple):
             # snt_ls = [sentence for sentence in txt.readlines()]
             for sentence in txt:
                 docbin.add(nlp(sentence))
-        
-        docbin.to_disk(f'C:\\Users\\mhess\\Documents\\UZH\\PCL\\PCL_PROJECT\\part_1\\{sys.argv[i][:-4]}.spacy')
+        docbin.to_disk(f"./{sys.argv[i][:-4]}.spacy")
         i += 1
+
+
 if __name__ == '__main__':
     txt_to_disk(files)
-
